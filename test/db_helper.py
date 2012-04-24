00b473d20ceb92237599c29a5296934324d864a2
Simon Malcolm <simon.malcolm@gmail.com>
2012-04-24 15:41:41 -0400

    refactor tests again

---
+++ test/db_helper.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+schema_file = 'db/create_tables.sql'
+test_data_file = 'db/test_data.sql'
+
+def connect(db = ":memory:"):
+    """return a db connection"""
+    connection = None
+    try:
+        connection = sqlite3.connect(db)
+    except sqlite3.Error, e:
+        print "Error: %s" % e.args[0]
+    return connection
+
+def close(connection):
+    """close a db connection"""
+    if connection:
+        connection.close();
+
+def repopulate(connection):
+    """recreate the test db from file"""
+    test_data = None
+    schema = None
+    with open(schema_file) as f:
+        schema = f.read()
+    with open(test_data_file) as f:
+        insert_data = f.read()
+    try:
+        connection.executescript(schema)
+        connection.executescript(insert_data)
+    except sqlite3.Error, e:
+        print "Error: %s" % e.args[0]
+    else:
+        connection.commit()
+
--- test/test_database.py
+++ test/test_database.py
@@ -1,67 +1,36 @@
 import sqlite3
-from nose.tools import with_setup
+import db_helper
 
-con = None
+test_db = None
 
 def setup():
     """create in-memory database"""
-    global con
+    global test_db
+    test_db = db_helper.connect()
 
-    try:
-        con = sqlite3.connect(':memory:')
-    except sqlite3.Error, e:
-        print "Error: %s" % e.args[0]
 
 def teardown():
     """destroy in-memory database"""
-    global con
-
-    if con:
-        con.close();
-        con = None
-
-def populate_db():
-    """insert test data"""
-    global con
-
-    insert_data, schema = None
-    with open("db/create_tables.sql") as f:
-        schema = f.read()
-    with open("db/test_data.sql") as f:
-        insert_data = f.read()
-    try:
-        con.executescript(schema)
-        con.executescript(insert_data)
-        con.commit()
-    except sqlite3.Error, e:
-        print "Error: %s" % e.args[0]
-
-def depopulate_db():
-    """dump test data"""
-    global con
-
-    delete_all = """DELETE FROM order_quantities;
-            DELETE FROM orders;
-            DELETE FROM ship_methods;
-            DELETE FROM authors;
-            DELETE FROM locations;
-            DELETE FROM publishers;
-            DELETE FROM bindings;
-            DELETE FROM books;
-            DELETE FROM distributors;"""
-
-    try:
-        con.executescript(delete_all)
-        con.commit()
-    except sqlite3.Error, e:
-        print "Error: %s" % e.args[0]
+    global test_db
+    db_helper.close(test_db);
 
 def test_connection():
-    global con
+    global test_db
     try:
-        cur = con.cursor()
+        cur = test_db.cursor()
         cur.execute("SELECT SQLITE_VERSION()")
         result = cur.fetchone()
     except sqlite3.Error, e:
         print "Error: %s" % e.args[0]
     assert result[0] == "3.7.10"
+
+def test_data_present():
+    global test_db
+    db_helper.repopulate(test_db)
+    try:
+        cur = test_db.cursor()
+        cur.execute("SELECT fax FROM distributors WHERE dist_name IS 'Oxford'")
+        result = cur.fetchone()
+    except sqlite3.Error, e:
+        print "Error: %s" % e.args[0]
+    assert result[0] == "(555)555-0002"
