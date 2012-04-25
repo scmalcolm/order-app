2a6332ed96b25c1de111e2f73abdc10758b61d32
Simon Malcolm <simon.malcolm@gmail.com>
2012-04-25 16:24:59 -0400

    add unit test and fuctionality for book insert to insert authors as well

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ class OrderDB:
         try:
             self.db_connection = sqlite3.connect(db_path)
         except sqlite3.Error, e:
-            print "Cannot connect to database: %s" % db_path
-            print "Error: %s" % e.args[0]
+            print "Cannot connect to database: {}",format(db_path)
+            print "Error: {}".format(e.args[0])
+
     def add_book(self, isbn13, title, binding, location, pub_name, authors = None):
         """add a new book to the database"""
         BOOK_INSERT_QUERY = """
@@ -20,6 +21,14 @@ class OrderDB:
         WHERE binding IS :binding
         LIMIT 1;
         """
+        AUTHOR_INSERT_QUERY = """
+        INSERT INTO authors (author, book_id)
+        SELECT
+        ?, book_id
+        FROM books
+        WHERE isbn13 IS ?
+        LIMIT 1;
+        """
         query_params = {
         'isbn13': isbn13,
         'title': title,
@@ -27,12 +36,11 @@ class OrderDB:
         'location': location,
         'pub_name': pub_name
         }
-        print 'Begin book insert: {}'.format(repr(query_params))
+        print 'Begin book insertion: {}'.format(repr(query_params))
         try:
-            cur = self.db_connection.cursor()
-            cur.execute(BOOK_INSERT_QUERY, query_params)
+            with self.db_connection as connection:
+                connection.execute(BOOK_INSERT_QUERY, query_params)   
+                connection.executemany(AUTHOR_INSERT_QUERY, [(n, isbn13) for n in authors])
+                print 'Book insert successful.'
         except sqlite3.Error, e:
-            print "Cannot insert book: %s" % isbn13
-            print "Error: %s" % e.args[0]
-        else:
-            self.db_connection.commit()
+            print "Error: {}".format(e.args[0])
--- test/test_model.py
+++ test/test_model.py
@@ -2,49 +2,76 @@ import sqlite3
 import db_helper
 from ..model import *
 
-def test_model_init():
-    db = OrderDB()
+test_db = "db/test.sqlite3"
 
-def test_book_insert():
-    test_db = "db/test.sqlite3"
+def setup():
+    global test_db
     con = db_helper.connect(test_db)
     db_helper.repopulate(con)
     con.commit()
     con.close()
 
-    BOOK_PARAMS = {
+def test_model_init():
+    db = OrderDB()
+
+def test_book_insert():
+    global test_db
+
+    TEST_PARAMS = {
     'isbn13': "9780061474095",
     'title': 'Anathem',
     'binding': 'Cloth',
     'location': 'Fiction',
-    'pub_name': 'William Morrow'}
+    'pub_name': 'William Morrow',
+    'authors': ['Neal Stephenson']}
+
+    BOOK_QUERY = """SELECT * FROM
+    books NATURAL JOIN
+    publishers NATURAL JOIN
+    bindings NATURAL JOIN
+    locations
+    WHERE isbn13 IS :isbn13;
+    """
+
+    AUTHOR_QUERY = """SELECT * FROM
+    books NATURAL JOIN
+    authors
+    WHERE isbn13 IS :isbn13;
+    """
 
     db = OrderDB(test_db)
-    db.add_book(**BOOK_PARAMS)
+    db.add_book(**TEST_PARAMS)
 
     con = db_helper.connect(test_db)
     try:
         cur = con.cursor()
-        cur.execute('SELECT * FROM books NATURAL JOIN publishers NATURAL JOIN bindings NATURAL JOIN locations WHERE isbn13 is :isbn13', BOOK_PARAMS)
-        #cur.execute('SELECT * FROM books NATURAL JOIN publishers')
-        con.commit()
-        rows = cur.fetchall()
+        cur.execute(BOOK_QUERY, TEST_PARAMS)
+        book_rows = cur.fetchall()
+        cur.execute(AUTHOR_QUERY, TEST_PARAMS)
+        author_rows = cur.fetchall()
     except sqlite3.Error, e:
-        print "Insertion test failure"
+        print "Insertion test failed to retrieve data!"
         print "Error: %s" % e.args[0]
-    assert rows is not None
-    print '{} rows'.format(repr(len(rows)))
-    assert len(rows) == 1
-    got_expected_result = True
-    for row in rows:
-        for column_name in BOOK_PARAMS.keys():
-            expected = BOOK_PARAMS[column_name]
-            try:
-                result = row[column_name]
-            except IndexError, e:
-                result = None
+    
+    assert book_rows is not None
+    assert len(book_rows) == 1
+    
+    book_columns_are_correct = True
+    for row in book_rows:
+        for column_name in TEST_PARAMS.keys():
+            if column_name == 'authors':
+                continue
+            expected = TEST_PARAMS[column_name]
+            result = row[column_name]
             print 'Column: {}, Expected: {}, Result: {}'.format(column_name, expected, result)
             if not expected == result:
-                got_expected_result = False
-    assert got_expected_result
+                book_columns_are_correct = False
+    assert book_columns_are_correct
 
+    assert author_rows is not None
+    assert len(author_rows) == 1
+
+    retrieved_authors = [row['author'] for row in author_rows]
+    for author in TEST_PARAMS['authors']:
+        retrieved_authors.remove(author)
+    assert len(retrieved_authors) == 0
