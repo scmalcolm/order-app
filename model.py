7086de25afe3ec912655bd6e463f719d93380fcd
Simon Malcolm <simon.malcolm@gmail.com>
2012-05-02 10:39:14 -0400

    implement single column book updates

--- model.py
+++ model.py
@@ -10,11 +10,11 @@ class OrderDB:
 
     def add_book(self, isbn13, title, binding, location, pub_name, authors = None):
         """add a new book to the database"""
-        BOOK_INSERT_QUERY = """INSERT INTO book_view
+        BOOK_INSERT_SQL = """INSERT INTO book_view
         (isbn13, title, binding, location, pub_name)
         VALUES
         (:isbn13, :title, :binding, :location, :pub_name);"""
-        AUTHOR_INSERT_QUERY = """INSERT INTO authors
+        AUTHOR_INSERT_SQL = """INSERT INTO authors
         (author, book_id)
         SELECT
         ?, book_id
@@ -29,21 +29,21 @@ class OrderDB:
         print 'Begin book insertion: {}'.format(repr(query_params))
         try:
             with self.db_connection as connection:
-                connection.execute(BOOK_INSERT_QUERY, query_params)   
-                connection.executemany(AUTHOR_INSERT_QUERY, [(n, isbn13) for n in authors])
+                connection.execute(BOOK_INSERT_SQL, query_params)   
+                connection.executemany(AUTHOR_INSERT_SQL, [(n, isbn13) for n in authors])
                 print 'Book insert successful.'
         except sqlite3.Error, e:
             print "Error: {}".format(e.args[0])
 
     def delete_book(self, isbn13):
         """remove a book from the database"""
-        BOOK_DELETE_QUERY = "DELETE FROM books WHERE isbn13 IS ?;"
+        BOOK_DELETE_SQL = "DELETE FROM books WHERE isbn13 IS ?;"
         with self.db_connection as connection:
-            connection.execute(BOOK_DELETE_QUERY, [isbn13])
+            connection.execute(BOOK_DELETE_SQL, [isbn13])
 
     def update_book(self, **params):
         """update a book record"""
-        BOOK_UPDATE_QUERY = """UPDATE book_view SET
+        BOOK_UPDATE_SQL = """UPDATE book_view SET
         isbn13 = :isbn13,
         title = :title,
         binding = :binding,
@@ -52,5 +52,34 @@ class OrderDB:
         WHERE isbn13 is :old_isbn13;"""
         
         with self.db_connection as connection:
-            connection.execute(BOOK_UPDATE_QUERY, params)
+            connection.execute(BOOK_UPDATE_SQL, params)
 
+    def update_isbn(self, old_isbn13, isbn13):
+        """update a book's 13-digit isbn"""
+        UPDATE_SQL = "UPDATE book_view SET isbn13 = ? WHERE isbn13 IS ?;"
+        with self.db_connection as connection:
+            connection.execute(UPDATE_SQL, (isbn13, old_isbn13))
+
+    def update_title(self, old_isbn13, title):
+        """update a book's title"""
+        UPDATE_SQL = "UPDATE book_view SET title = ? WHERE isbn13 IS ?;"
+        with self.db_connection as connection:
+            connection.execute(UPDATE_SQL, (title, old_isbn13))
+
+    def update_binding(self, old_isbn13, binding):
+        """update a book's binding"""
+        UPDATE_SQL = "UPDATE book_view SET binding = ? WHERE isbn13 IS ?;"
+        with self.db_connection as connection:
+            connection.execute(UPDATE_SQL, (binding, old_isbn13))
+
+    def update_location(self, old_isbn13, location):
+        """update a book's location"""
+        UPDATE_SQL = "UPDATE book_view SET location = ? WHERE isbn13 IS ?;"
+        with self.db_connection as connection:
+            connection.execute(UPDATE_SQL, (location, old_isbn13))
+
+    def update_publisher(self, old_isbn13, publisher):
+        """update a book's publisher"""
+        UPDATE_SQL = "UPDATE book_view SET pub_name = ? WHERE isbn13 IS ?;"
+        with self.db_connection as connection:
+            connection.execute(UPDATE_SQL, (publisher, old_isbn13))
