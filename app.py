bfe524d8b03394ee327c1356d2b17e6c2c9341e5
Simon Malcolm <simon.malcolm@gmail.com>
2012-05-29 16:49:00 -0400

    display book list - selecting a list item populates the edt fields

--- app.py
+++ app.py
@@ -66,11 +66,43 @@ class BookList(layout.BookListFrame):
                       ColumnDefn("Binding",   valueGetter="binding"),
                       ColumnDefn("Location",  valueGetter="location")]
         self.bookList.SetColumns(columnList)
-        books = wx.GetApp().model.get_books()
-        self.bookList.SetObjects(books)
+        self.selectedBook = None
+        self.books = wx.GetApp().model.get_books()
+        self.bookList.SetObjects(self.books)
+        self.authorList.SetColumns([ColumnDefn("Name", width=50, valueGetter="author")])
 
     def OnOKButtonClick(self, event):
         self.Close()
+
+    def OnDeleteBook(self, event):
+        pass
+
+    def OnSelectBook(self, event):
+        isbn13 = self.bookList.GetSelectedObject()['isbn13']
+        self.selectedBook = wx.GetApp().model.get_book(isbn13)
+        self.RefreshEditPanel()
+        self.bookEditPanel.Enable(True)
+
+    def OnDeselectBook(self, event):
+        self.selectedBook = None
+        self.ClearEditPanel()
+        self.bookEditPanel.Enable(False)
+
+    def RefreshEditPanel(self):
+        self.isbnText.SetValue(self.selectedBook['isbn13'])
+        self.titleText.SetValue(self.selectedBook['title'])
+        self.authorList.SetObjects([{'author': author} for author in self.selectedBook['authors']])
+        self.publisherCombo.SetValue(self.selectedBook['pub_name'])
+        self.bindingCombo.SetValue(self.selectedBook['binding'])
+        self.locationCombo.SetValue(self.selectedBook['location'])
+
+    def ClearEditPanel(self):
+        self.isbnText.SetValue('')
+        self.titleText.SetValue('')
+        self.authorList.SetObjects([])
+        self.publisherCombo.SetValue('')
+        self.bindingCombo.SetValue('')
+        self.locationCombo.SetValue('')
         
 class OrderApp(wx.App):
 
