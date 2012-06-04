36b582ad1c3ed2758f554cdb22502d3ed5619dfb
Simon Malcolm <simon.malcolm@gmail.com>
2012-06-04 12:14:21 -0400

    fix import error in new subpackage structure

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-from view.app import OrderApp
+import wx
+import model
+import view.app
+from ObjectListView import ColumnDefn, ObjectListView
 
-app = OrderApp(False)
-app.MainLoop()
+db_path = "/Users/bmbr/db/test.sqlite3"
+
+class OrderApp(wx.App):
+
+    def OnInit(self):
+        self.model = model.OrderDB(db_path)
+        self.Bind(wx.EVT_ACTIVATE_APP, self.OnActivate)
+        top = view.app.MainWindow(None)
+        self.SetTopWindow(top)
+        top.Show()
+        return True
+
+    def BringWindowsToFront(self):
+        try:
+            top = self.GetTopWindow()
+            top.Raise()
+        except:
+            pass
+
+    def OnActivate(self, event):
+        if event.GetActive():
+            self.BringWindowsToFront()
+        event.Skip()
+
+    def MacOpenFile(self, filename):
+        """Called for files dropped on dock icon or opened with context menu"""
+        pass
+
+    def MacReopenApp(self):
+        """Called when the dock icon is clicked, maybe other times?"""
+        self.BringWindowsToFront()
+
+    def MacNewFile(self):
+        pass
+
+    def MacPrintFile(self, file_path):
+        pass
+
+if __name__ == "__main__":
+    app = OrderApp(False)
+    app.MainLoop()
--- view/menu.py
+++ view/menu.py
@@ -5,10 +5,7 @@ Copyright Simon Malcolm 2012
 '''
 import wx
 import layout
-import model
 from ObjectListView import ColumnDefn, ObjectListView
-
-db_path = "/Users/bmbr/db/test.sqlite3"
 
 class MainWindow(layout.MainFrame):
 
@@ -109,42 +106,3 @@ class BookList(layout.BookListFrame):
         old_isbn = self.selectedBook['isbn13']
         wx.GetApp().model.update_isbn(old_isbn, new_isbn)
         
-class OrderApp(wx.App):
-
-    def OnInit(self):
-        self.model = model.OrderDB(db_path)
-        self.Bind(wx.EVT_ACTIVATE_APP, self.OnActivate)
-        top = MainWindow(None)
-        self.SetTopWindow(top)
-        top.Show()
-        return True
-
-    def BringWindowsToFront(self):
-        try:
-            top = self.GetTopWindow()
-            top.Raise()
-        except:
-            pass
-
-    def OnActivate(self, event):
-        if event.GetActive():
-            self.BringWindowsToFront()
-        event.Skip()
-
-    def MacOpenFile(self, filename):
-        """Called for files dropped on dock icon or opened with context menu"""
-        pass
-
-    def MacReopenApp(self):
-        """Called when the dock icon is clicked, maybe other times?"""
-        self.BringWindowsToFront()
-
-    def MacNewFile(self):
-        pass
-
-    def MacPrintFile(self, file_path):
-        pass
-
-if __name__ == "__main__":
-    app = OrderApp(False)
-    app.MainLoop()
