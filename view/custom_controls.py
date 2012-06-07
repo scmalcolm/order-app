f94d7585b94aef82b912abf54b6bcea753ca568a
Simon Malcolm <simon.malcolm@gmail.com>
2012-06-07 11:05:24 -0400

    add module for customizations to wx controls

---
+++ view/custom_controls.py
@@ -0,0 +1,20 @@
+import wx
+
+class AutoComboBox(wx.ComboBox) :
+    def __init__(self, *args, **kwargs):
+        super(AutoComboBox, self).__init__(*args, **kwargs)
+        
+        self.Bind(wx.EVT_CHAR, self.OnChar)
+
+    def OnChar(self, event):
+        print "OnChar"
+        keycode = event.GetString()
+        
+        print "Keycode: {}".format(keycode)
+
+        event.Skip()
+
+    def OnText(self, event):
+        print "OnText"
+
+        event.Skip()
